2b469f8c9e8b48eb9cd6a4feb0e25926ff5404d8
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2022-05-20 09:19:58 +0300

    Add styles tournaments create page. Improve responsive design

--- football-tournament/src/pages/Tournament/create.js
+++ football-tournament/src/pages/Tournament/create.js
@@ -2,7 +2,39 @@
 const Create = () => {
   return (
     <div>
-      Create
+      <form className='create-tournament-form'>
+        <div className='row'>
+          <input className='col' type='text' placeholder='Name'/>
+        </div>
+
+        <div className='row country'>
+          <select className="col">
+            <option>Select country</option>
+            <option>Bulgaria</option>
+            <option>USA</option>
+            <option>UK</option>
+            <option>France</option>
+            <option>Spain</option>
+            <option>Italy</option>
+          </select>
+        </div>
+
+        <div className='row'>
+          <select className='col'>
+            <option>Select category</option>
+            <option>Junior</option>
+            <option>Adult</option>
+          </select>
+        </div>
+
+        <div className='row'>
+          <input className='col' type='number' placeholder='Price'/>
+        </div>
+
+        <div>
+          <button className='create-btn'>Create</button>
+        </div>
+      </form>
     </div>
   )
 }
