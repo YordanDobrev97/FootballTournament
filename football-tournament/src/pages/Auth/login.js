36e7fdf604d8473a29fb27f11151ed592a8baebc
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2022-05-28 12:49:40 +0300

    Add load spinners

--- football-tournament/src/pages/Auth/login.js
+++ football-tournament/src/pages/Auth/login.js
@@ -1,6 +1,7 @@
 import { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useCookies } from 'react-cookie'
+import ClipLoader from "react-spinners/ClipLoader"
 
 import Form from '../../components/Auth/Form'
 import Input from '../../components/Auth/Input'
@@ -14,17 +15,20 @@ const Login = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
+  const [loading, setLoading] = useState(false)
   const [_, setCookie] = useCookies(['jwt'])
 
   const context = useContext(AuthContext)
   const navigation = useNavigate()
 
   const onSubmit = async () => {
+    setLoading(true)
     try {
       const res = await authenticate('login', 'POST', DEFAULT_HEADERS, { username, password })
       const token = await res.json()
       setCookie('jwt', token)
       context.setAuthenticated(true)
+      setLoading(false)
       navigation('/home')
     } catch (error) {
       setErrorMessage('Login failed')
@@ -53,7 +57,11 @@ const Login = () => {
           />
           <button onClick={onSubmit} className='auth-btn'>Login</button>
         </Form>
-
+        {loading && (
+          <div className='loader'>
+            <ClipLoader color='#ffff' loading={loading} size={150} />
+          </div>
+        )}
         {errorMessage && (
           <div className='error'>{errorMessage}</div>
         )}
--- football-tournament/src/pages/Team/index.js
+++ football-tournament/src/pages/Team/index.js
@@ -1,6 +1,8 @@
 import { useState, useEffect, useMemo } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { useCookies } from 'react-cookie'
+import ClipLoader from "react-spinners/ClipLoader"
+
 import Pagination from "../../components/Pagination"
 import { api } from "../../utils/request"
 import parseJwt  from '../../utils/jwtParser'
@@ -14,13 +16,14 @@ const Team = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [cookies] = useCookies(['jwt'])
   const navigation = useNavigate()
+  const [loading, setLoading] = useState(false)
   const user = parseJwt(cookies?.jwt)
 
   useEffect(() => {
     if (!cookies?.jwt) {
       navigation('/login')
     }
-
+    setLoading(true)
     api
       .get("teams/all", {
         "Content-Type": "application/json",
@@ -29,6 +32,7 @@ const Team = () => {
       .then((r) => r.json())
       .then((data) => {
         setTeams(data);
+        setLoading(false)
       });
   }, []);
 
@@ -56,7 +60,12 @@ const Team = () => {
         </thead>
 
         <tbody>
-          {currentTableData && currentTableData.map((team) => {
+        {loading ? (
+          <div className='loader'>
+            <ClipLoader color='#ffff' loading={loading} size={150} />
+          </div>
+        ) : (
+          currentTableData.map((team) => {
             return (
               <tr className="top">
                 <td>{team.id}</td>
@@ -72,7 +81,8 @@ const Team = () => {
                 </td>
               </tr>
             )
-          })}
+          })
+        )}
         </tbody>
       </table>
       <Pagination
